9ffe95b0cab9c3f5f5dc60cb0dfa28f0f14dfd2c
nerochen25 <nerochen25@berkeley.edu>
2018-11-13 15:57:21 -0800

    Add cats project folder

---
+++ cats/app/models/cat.rb
@@ -0,0 +1,27 @@
+class Cat < ApplicationRecord
+  COLORS = ['Brown', 'Black', 'White', 'Yellow', 'Blue', 'Green']
+  validates :color,
+    presence: true,
+    inclusion: COLORS
+  
+  SEX = ['M', 'F']  
+  validates :sex,
+    presence: true,
+    inclusion: SEX
+  
+  validates :name,
+    presence: true 
+
+  validates :birth_date,
+    presence: true
+    
+    
+  def age
+    now = Time.now.utc.to_date
+    if now.month > self.birth_date.month
+      return now.year - (self.birth_date.year - 1)
+    else
+      now.year - self.birth_date.year
+    end
+  end
+end
